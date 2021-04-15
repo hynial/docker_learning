5fc145d6fed09b59d180f7bf195f3e3848ae7137
hynial.chen <hynials@gmail.com>
2021-04-15 22:56:24 +0800

    init.sql

---
+++ docker_php/nginx/init/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS demo DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
+
+CREATE USER 'smer'@'localhost' IDENTIFIED BY '12345lQ!';
+CREATE USER 'smer'@'%' IDENTIFIED BY '12345lQ!';
+GRANT ALL PRIVILEGES ON demo.* TO 'smer'@'%';
+FLUSH PRIVILEGES;
+
+SET GLOBAL time_zone = '+8:00';
+
+USE demo;
