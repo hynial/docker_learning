212c150707e82213dc812d8858751b649a3b5c1b
hynial.chen <hynials@gmail.com>
2021-04-15 23:06:33 +0800

    setting

--- docker_php/nginx/init/init.sql
+++ docker_php/nginx/init/init.sql
@@ -5,6 +5,10 @@ CREATE USER 'smer'@'%' IDENTIFIED BY '12345lQ!';
 GRANT ALL PRIVILEGES ON demo.* TO 'smer'@'%';
 FLUSH PRIVILEGES;
 
+SET NAMES utf8;
+SET time_zone = '+08:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 SET GLOBAL time_zone = '+8:00';
 
 USE demo;
